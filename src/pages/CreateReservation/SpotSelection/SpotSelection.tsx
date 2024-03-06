ce222501b24aa79054bd6e3143bb441af0a77c32
Daniel99Petrov <118048921+Daniel99Petrov@users.noreply.github.com>
2024-03-06 16:23:59 +0200

    Update SpotSelection.tsx
    Remove commented code block

--- src/pages/CreateReservation/SpotSelection/SpotSelection.tsx
+++ src/pages/CreateReservation/SpotSelection/SpotSelection.tsx
@@ -8,52 +8,6 @@ export default function SpotSelection() {
     const { formik, navigate, spotProps, startPeriodDate, startPeriodTime, endPeriodDate, endPeriodTime } =
         useReservSpot();
 
-    // return (
-    //     <Modal>
-    //         <h3>{spotProps.name}</h3>
-    //         <p>Spot type: {spotProps.spotType}</p>
-    //         <p>Description: {spotProps.description}</p>
-    //         {spotProps.spotType === 'Office desk' || spotProps.spotType === 'Parking place' ? (
-    //             <div>
-    //                 Reservation period:
-    //                 <p>from: {startPeriodDate}</p>
-    //                 <p>to: {endPeriodDate}</p>
-    //                 {selectedUser && <p>user: {selectedUser.email}</p>}
-    //             </div>
-    //         ) : (
-    //             <div>
-    //                 Reservation period: on {startPeriodDate}
-    //                 <p>from: {startPeriodTime}</p>
-    //                 <p>to: {endPeriodTime}</p>
-    //                 {selectedUser && <p>user: {selectedUser.email}</p>}
-    //             </div>
-    //         )}
-    //         <FormStyled onSubmit={formik.handleSubmit}>
-    //             <InputField
-    //                 type="text"
-    //                 name={'comment'}
-    //                 id={'comment'}
-    //                 label="Add comment:"
-    //                 onChange={formik.handleChange}
-    //             />
-    //             {formik.errors.comment && formik.touched.comment ? <div>{formik.errors.comment}</div> : null}
-    //             <FormButtonsContainer>
-    //                 <BaseButton type={'submit'}>Add reservation</BaseButton>
-    //                 <BaseButton
-    //                     type="button"
-    //                     className="close-btn"
-    //                     onClick={() => {
-    //                         navigate(-1);
-    //                     }}
-    //                 >
-    //                     Close
-    //                 </BaseButton>
-    //             </FormButtonsContainer>
-    //             {formik.errors.error ? <div>{formik.errors.error}</div> : null}
-    //         </FormStyled>
-    //     </Modal>
-    // );
-
     return (
         <Modal>
             {spotProps && (
