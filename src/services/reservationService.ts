bcd1ec207bffaab9c3c3b932153a0e7f6adfc09f
Daniel Petrov <daniplpetrov@gmail.com>
2024-03-06 12:50:42 +0200

    feat(reservation.service):add endpoint to get future reservations by user and location

--- src/services/reservationService.ts
+++ src/services/reservationService.ts
@@ -1,67 +1,96 @@
-import { MultipleReservations } from '../pages/CreateReservation/SpotSelection/SpotSelection.static';
-import { Reservation } from '../static/types';
+import { MultipleReservations } from "../pages/CreateReservation/SpotSelection/SpotSelection.static";
+import { Reservation } from "../static/types";
 
-import { endpoints } from '../static/endpoints';
-import { get, del, post } from './fetchService';
+import { endpoints } from "../static/endpoints";
+import { get, del, post } from "./fetchService";
 
 const getAllReservations = async (): Promise<Reservation[]> => {
-    const response = await get(endpoints.reservation, {});
-    return response;
+  const response = await get(endpoints.reservation, {});
+  return response;
 };
 
-const getPastReservationsByUserId = async (id: string | undefined): Promise<Reservation[]> => {
-    const response = await get(`${endpoints.getPastReservationsByUser}${id}`, {});
-    return response;
+const getPastReservationsByUserId = async (
+  id: string | undefined
+): Promise<Reservation[]> => {
+  const response = await get(`${endpoints.getPastReservationsByUser}${id}`, {});
+  return response;
 };
 
-const getCurrentReservationsByUserId = async (id: string | undefined): Promise<Reservation[]> => {
-    const response = await get(`${endpoints.getCurrentReservationsByUser}${id}`, {});
-    return response;
+const getCurrentReservationsByUserId = async (
+  id: string | undefined
+): Promise<Reservation[]> => {
+  const response = await get(
+    `${endpoints.getCurrentReservationsByUser}${id}`,
+    {}
+  );
+  return response;
 };
 
-const getFutureReservationsByUserId = async (id: string | undefined): Promise<Reservation[]> => {
-    const response = await get(`${endpoints.getFutureReservationsByUser}${id}`, {});
-    return response;
+const getFutureReservationsByUserId = async (
+  id: string | undefined
+): Promise<Reservation[]> => {
+  const response = await get(
+    `${endpoints.getFutureReservationsByUser}${id}`,
+    {}
+  );
+  return response;
+};
+
+const getFutureReservationsByUserIdAndLocation = async (
+  userId: string | undefined,
+  locationId: string | undefined
+): Promise<Reservation[]> => {
+  const response = await get(
+    `${endpoints.getFutureReservationsByUserAndLocation}${userId}/${locationId}`,
+    {}
+  );
+  return response;
 };
 
 const getResevationsBySpot = async (spotId: string): Promise<Reservation[]> => {
-    const response = await get(`${endpoints.getResevationsBySpotId}/${spotId}`, {});
-    return response;
+  const response = await get(
+    `${endpoints.getResevationsBySpotId}/${spotId}`,
+    {}
+  );
+  return response;
 };
 
 const checkReservation = async ({
+  spotId,
+  start,
+  end,
+  comment,
+  userId,
+  modifiedBy,
+}: Reservation): Promise<Reservation> => {
+  return await post(`${endpoints.checkReservation}`, {
     spotId,
     start,
     end,
     comment,
     userId,
     modifiedBy,
-}: Reservation): Promise<Reservation> => {
-    return await post(`${endpoints.checkReservation}`, {
-        spotId,
-        start,
-        end,
-        comment,
-        userId,
-        modifiedBy,
-    });
+  });
 };
 
-const createReservation = async ({ reservations }: MultipleReservations): Promise<MultipleReservations> => {
-    return await post(`${endpoints.createReservation}`, { reservations });
+const createReservation = async ({
+  reservations,
+}: MultipleReservations): Promise<MultipleReservations> => {
+  return await post(`${endpoints.createReservation}`, { reservations });
 };
 
 const deleteReservation = async (reservationId: string): Promise<void> => {
-    await del(`${endpoints.reservation}/${reservationId}`, {});
+  await del(`${endpoints.reservation}/${reservationId}`, {});
 };
 
 export {
-    getAllReservations,
-    getPastReservationsByUserId,
-    getCurrentReservationsByUserId,
-    getFutureReservationsByUserId,
-    getResevationsBySpot,
-    checkReservation,
-    createReservation,
-    deleteReservation,
+  getAllReservations,
+  getPastReservationsByUserId,
+  getCurrentReservationsByUserId,
+  getFutureReservationsByUserId,
+  getFutureReservationsByUserIdAndLocation,
+  getResevationsBySpot,
+  checkReservation,
+  createReservation,
+  deleteReservation,
 };
