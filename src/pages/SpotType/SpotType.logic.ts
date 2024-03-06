3b529c57ea23ecfaa4799db34cd62e5e8e2c6923
Daniel Petrov <daniplpetrov@gmail.com>
2024-03-06 11:48:48 +0200

    feat(SpotType.logic): add hook to get future reservations by user and location

--- src/pages/SpotType/SpotType.logic.ts
+++ src/pages/SpotType/SpotType.logic.ts
@@ -1,25 +1,44 @@
-import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
-import { getLocation } from '../../services/locationService';
-import useToken from '../../hooks/Token/Token.hook';
+import { useQuery } from "react-query";
+import { useParams } from "react-router-dom";
+import { getLocation } from "../../services/locationService";
+import useToken from "../../hooks/Token/Token.hook";
+import { getFutureReservationsByUserIdAndLocation } from "../../services/reservationService";
 
 const useChoseLocation = () => {
-    const { id } = useParams();
-    const decodedToken = useToken();
+  const { id } = useParams();
+  const decodedToken = useToken();
 
-    const { id: userId } = decodedToken || {};
+  const { id: userId } = decodedToken || {};
 
-    const {
-        data: singleLocation,
-        isLoading,
-        error,
-    } = useQuery(['location', id], () => {
-        if (id) {
-            return getLocation(id);
-        }
-    });
+  const {
+    data: singleLocation,
+    isLoading,
+    error,
+  } = useQuery(["location", id], () => {
+    if (id) {
+      return getLocation(id);
+    }
+  });
 
-    return { singleLocation, isLoading, error, userId };
+  return { singleLocation, isLoading, error, userId };
 };
 
-export default useChoseLocation;
+const useFutureReservationsByUserIdAndLocation = (
+  userId: string | undefined
+) => {
+  const { id } = useParams();
+  const {
+    data: reservations,
+    refetch,
+    isLoading,
+  } = useQuery(["futureReservationsByUserId", userId], () =>
+    getFutureReservationsByUserIdAndLocation(userId, id)
+  );
+  return {
+    futureReservations: reservations,
+    futureReservationsRefetch: refetch,
+    areFutureReservationsLoading: isLoading,
+  };
+};
+
+export { useChoseLocation, useFutureReservationsByUserIdAndLocation };
